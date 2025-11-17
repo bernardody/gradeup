3d6897f365a2a6c579cd1736ff07623fcdeac55c
phelipe <phelegal88@gmail.com>
2025-11-17 16:44:31 -0300

    feat(front): login com redirecionamento pelo type do usuario adicionado.

--- app/src/pages/Login/Login.tsx
+++ app/src/pages/Login/Login.tsx
@@ -1,43 +1,71 @@
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState(""); 
 
-  const handleLogin = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Aqui depois tu vai validar o login com a API
-    navigate("/HomeStudent"); 
-  };
+  const handleLogin = async (e: React.FormEvent) => {
+  e.preventDefault();
 
+  try {
+    const response = await fetch("http://localhost:8080/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-  // e so pra eu ficar testando dps a gente remove ok
-  const validacaoTESTE = (e: React.FormEvent) => {
-  e.preventDefault(); 
+    if (!response.ok) {
+      alert("Erro no login — veja o console.");
+      return;
+    }
 
-  const tipoInput = document.getElementById("tipo") as HTMLInputElement;
-  const tipo = tipoInput?.value;
+    const data = await response.json();
 
-  if (tipo === "aluno") {
-    navigate("/HomeStudent");
-  } else if (tipo === "professor") {
-    navigate("/HomeTeacher");
-  } else {
-    alert("Tipo de usuário inválido. Use 'aluno' ou 'professor'.");
-  };
+    if (data.type === "STUDENT") {
+      navigate("/HomeStudent", { state: data });
+    } else if (data.type === "TEACHER") {
+      navigate("/HomeTeacher", { state: data });
+    } else if (data.type === "ADMIN") {
+      navigate("/HomeAdmin", { state: data });
+    }
+
+  } catch (error) {
+    console.error("ERRO CATCH:", error);
+    alert("Erro no servidor.");
   }
+};
 
   return (
     <div className="login-page">
       <div className="login-box">
         <h1>Bem-Vindo ao GradeUP</h1>
 
-        <form onSubmit={validacaoTESTE}> 
-          <label>Usuário</label>
-          <input id="tipo" className="login-input" type="text" placeholder="Digite seu nome" required />
+        <form onSubmit={handleLogin}> 
+          <label>Email</label>
+          <input 
+            className="login-input" 
+            type="email" 
+            placeholder="Digite seu email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required 
+          />
 
           <label>Senha</label>
-          <input className="login-input"  type="password" placeholder="Digite sua senha" required />
+          <input 
+            className="login-input"  
+            type="password" 
+            placeholder="Digite sua senha" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required 
+          />
 
           <button type="submit">Login</button>
         </form>
