91a40b1a06aa142d1d4fa294b9f0f6513b51e8cd
phelipe <phelegal88@gmail.com>
2025-11-18 16:58:37 -0300

    feat(front): implementacao do grafico da tela do professor

--- app/src/pages/Home/HomeTeacher.tsx
+++ app/src/pages/Home/HomeTeacher.tsx
@@ -1,15 +1,25 @@
 import TeacherSideBar from "../SideBar/Teacher-SideBar";
 import HomeBase from "./HomeBase";
 import "./css/HomeTeacher.css";
+import "./css/HomeBase.css";
+import Graphic from "../Home/Graphic"
 
 export default function HomeTeacher() {
     return (
     <div className="hometeacher">
-        <div className="base">
-            <HomeBase />
-        </div>
         <div className="sideBar">
             <TeacherSideBar />
+        </div>
+        <div className="base">
+            <HomeBase />
+            <div className="graphic-container">
+                <div id="title">
+                    <h1>Visao geral das notas de suas turmas</h1>
+                </div>
+                <div className="graphic">
+                    <Graphic />
+                </div>
+            </div>
         </div>
     </div>
     )
