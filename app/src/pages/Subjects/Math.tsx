f0519dc5f796ff11e6ef710e3d12b6a30c97499f
Arthur <arthurmullerg@gmail.com>
2025-11-19 16:41:12 -0300

    feat(front): implement SubjectCard component and update Subjects page layout

---
+++ app/src/pages/Subjects/Math.tsx
@@ -0,0 +1,14 @@
+import StudentSideBar from "../SideBar/Student-SideBar";
+import "../Subjects/css/subjects.css";
+
+export default function Matematica(){
+    return (
+        <div className="subjects-container">
+
+
+         <div className="sideBar">
+                    <StudentSideBar />
+                </div> 
+        </div>
+    )
+}
